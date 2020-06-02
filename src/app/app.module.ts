05488591d00fad20603885b9f1f3f76cfd490f8e
Nishant Pratap <singhnishant411@gmail.com>
2020-06-02 17:00:07 +0530

    basic table added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TableComponent } from './table/table.component';
+import { SideDetailsComponent } from './side-details/side-details.component';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent,
+    SideDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    FormsModule,
+    CommonModule,
+    ReactiveFormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/table/table.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit ,PipeTransform } from '@angular/core';
+import { DecimalPipe } from '@angular/common';
+import { FormControl } from '@angular/forms';
+
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+interface Data {
+  date: string;
+  listName: string;
+  entity: number;
+  action: string;
+}
+const COUNTRIES: Data[] = [
+  {
+    date: '26/8',
+    listName: 'Competitive no',
+    entity: 0,
+    action: ''
+  },
+  {
+    date: '26/9',
+    listName: 'My vendors',
+    entity: 2,
+    action: ''
+  }
+  
+];
+
+function search(text: string, pipe: PipeTransform): Data[] {
+  return COUNTRIES.filter(country => {
+    const term = text.toLowerCase();
+    return country.date.toLowerCase().includes(term)
+    ||country.listName.toLowerCase().includes(term)
+      
+        || pipe.transform(country.entity).includes(term);
+  });
+}
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.scss'],
+  providers: [DecimalPipe]
+})
+export class TableComponent implements OnInit {
+
+  
+  countries$: Observable<Data[]>;
+  filter = new FormControl('');
+  constructor(pipe: DecimalPipe) { 
+    this.countries$ = this.filter.valueChanges.pipe(
+      startWith(''),
+      map(text => search(text, pipe))
+    );
+  }
+
+  ngOnInit(): void {
+  }
+
+  details(i){
+
+  }
+}
